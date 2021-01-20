6674e7ba19a5c70a5a3390e8b3abc9dcc3f6455f
bchareyre <bruno.chareyre@grenoble-inp.fr>
2021-01-20 21:37:46 +0100

    fix '<' vs '<=' inconsistency between python test and collider behavior

--- scripts/checks-and-tests/checks/colliderTorture.py
+++ scripts/checks-and-tests/checks/colliderTorture.py
@@ -107,9 +107,9 @@ def signature():
         sumRad=yade.math.toHP1(0) # after https://gitlab.com/yade-dev/trunk/-/merge_requests/599 is merged it can be: yade.math.Real(0)
         if i.id1 > 0: sumRad+=O.bodies[i.id1].shape.radius
         if i.id2 > 0: sumRad+=O.bodies[i.id2].shape.radius
-        away = abs(d[0])>sumRad or  abs(d[1])>sumRad or abs(d[2])>sumRad
+        away = abs(d[0])>=sumRad or  abs(d[1])>=sumRad or abs(d[2])>=sumRad
         
-        if (not away): #isReal is for sphere-box
+        if (not away or i.isReal): #isReal is for sphere-box
             sig+=i.id1+i.id2
             count+=1
     return count,sig
