a403cc5f371d6b94a7390a5cd67fef1fc26a90ad
bchareyre <bruno.chareyre@grenoble-inp.fr>
2021-01-21 17:33:58 +0100

    temptative final MPI version of Case1 benchmark

--- examples/DEM8Benchmark/Case1_SiloFlow.py
+++ examples/DEM8Benchmark/Case1_SiloFlow.py
@@ -4,10 +4,10 @@
 
 # Benchmark of basic performance of open-source DEM simulation systems
 # Case 1: Silo flow
+# Units: SI (m, N, Pa, kg, sec)
 
 
-
-numThreads=1
+numThreads=4
 reportTiming=False
 
 # Configure MPI module if needed
@@ -18,36 +18,23 @@ else:
     print("yade is compiled without MPI support, numThreads>1 ignored")
     numThreads = 1
 
-# The stl file is in mm -> We can keep the units below or scale the stl file to meters (ymport.stl does not have a scale parameter, but I can edit the .stl file manually)
-
-# -------------------------------------------------------------------- #
-# Units: N/mm/MPa/ton
-# 	force:   1 N
-# 	length:  1 mm = 1e-3 m
-# 	stress:  1 MPa = 1 N/mm^2 = 1e6 Pa = 1e-3 GPa
-#	mass:    1 ton = 1e3 kg
-#	density: 1 ton/mm^3 = 1e-12 kg/m^3
 
 # -------------------------------------------------------------------- #
 # Input Data -> Define Material and Orifice size. Uncomment the prefered choice
 
-#FIXME: most of what follows don't have to be guarded by rank==0
-fileName='SiloLargeOrifice'
+fileName='LargeOrifice'
 #fileName='SiloSmallOrifice'
-if fileName=='SiloLargeOrifice':
-    z=70	# This is the height of the lowest point of the funnel (at the orifice), measuring from the lowest cylindrical cross section of the silo
-elif fileName=='SiloSmallOrifice':
-    z=80
-
-
-particleRadius = 8 # 2mm in the benchmark, bigger for less particles and quicker simulations
+if fileName=='LargeOrifice':
+    z=55.4222/1000. 	# This is the height of the lowest point of the funnel (at the orifice), measuring from the lowest cylindrical cross section of the silo
+elif fileName=='SmallOrifice':
+    z=59.3008/1000.
 
 granularMaterial='M1'
 #granularMaterial='M2'
 
 # -------------------------------------------------------------------- #
 # Materials
-Steel = O.materials.append(FrictMat(young=210e3,poisson=0.2,density=7200e-12,label='Steel'))
+Steel = O.materials.append(FrictMat(young=210e9,poisson=0.2,density=7200,label='Steel'))
 
 # -------------------------------------------------------------------- #
 # Asign values based on the Material and Orifice size
@@ -59,13 +46,13 @@ e_M2_M2=0.4;	f_M2_M2=0.4
 e_M2_St=0.4;	f_M2_St=0.2
 
 if granularMaterial=='M1':
-    M1=O.materials.append(FrictMat(young=1.0e3,poisson=0.2,density=2500e-12,label='M1'))
+    M1=O.materials.append(FrictMat(young=1.0e9,poisson=0.2,density=2500,label='M1'))
     e_gg=e_M1_M1	# Coefficient of restitution (e) between granular material (g) and granular material (g)
     f_gg=f_M1_M1	# Coefficient of friction (f)...
     e_gs=e_M1_St	# Coefficient of restitution (e) between granular material (g) and steel (s)
     f_gs=f_M1_St	# Coefficient of friction (f)...
 elif granularMaterial=='M2':
-    M2=O.materials.append(FrictMat(young=0.5e3,poisson=0.2,density=2000e-12,label='M2'))
+    M2=O.materials.append(FrictMat(young=0.5e9,poisson=0.2,density=2000,label='M2'))
     e_gg=e_M2_M2
     f_gg=f_M2_M2
     e_gs=e_M2_St
@@ -79,7 +66,7 @@ F_gs=atan(f_gs) # Friction Angle between granular material (g) and steel (s)
 ## Engines 
 O.engines=[
     ForceResetter(),
-    InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()],label="collider"),
+    InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()],smartInsertErase=False,label="collider"),
     InteractionLoop(
         [Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
         [Ip2_FrictMat_FrictMat_MindlinPhys(
@@ -97,49 +84,32 @@ O.engines=[
 # This condition is not abolutely necessary but it would be inelegant to
 # download *.stl and generate densePack N times when we need it done only on master (centralized scene method)
 if not mpi or mp.rank==0:
-
-    # -------------------------------------------------------------------- #
-    # Generate initial packing. Choose among regularOrtho, regularHexa or randomDensePack (which I think is best).
-
-    ## Using regularOrtho
-    #sp=pack.regularOrtho(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,305),radius),radius=2,gap=r*1/10.,material=granularMaterial)
-
-    ## Using regularHexa
-    #sp=pack.regularHexa(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,215),radius),radius=2,gap=r*1/10.,material=granularMaterial)  
-
-    # Using randomDensePack
-    sp=pack.randomDensePack(pack.inCylinder((0,0,0),(0,0,254),100),radius=particleRadius,spheresInCell=500,returnSpherePack=False,material=granularMaterial,seed=1) 
-
-    # -------------------------------------------------------------------- #
-    # Sort packing in ascending Z coordinates and delete excess particles to achieve sample size of 122k
-    zValues=[]
-    for s in sp:
-        zValues.append(s.state.pos[2])
-
-    from operator import itemgetter
-    indices, zValues_sorted = zip(*sorted(enumerate(zValues), key=itemgetter(1)))
-    list(zValues)
-    list(indices)
-
-    sp_new=[]
-    for i in range(0,len(sp)):
-        sp_new.append(sp[indices[i]])
-
-    Nspheres=122000
-    sp_new=sp_new[0:Nspheres]
-
     from yade import ymport
-    if not os.path.exists(fileName+'.stl'):
-        print("Downloading mesh file")
+    wallFile='Case1_SiloFlow_Walls_'+fileName+'.txt'
+    sphereFile='Case1_SiloFlow_PartCoordinates_'+fileName+'.txt'
+    
+    hasInputSpheres = os.path.exists(sphereFile)
+    if not hasInputSpheres:
+        print("Downloading sphere file",sphereFile)
         try:
-            os.system('wget http://yade-dem.org/publi/data/DEM8/'+fileName+'.stl')
+            os.system('wget http://yade-dem.org/publi/data/DEM8/'+sphereFile)
         except:
-            print("** probably no internet connection, grab the *.stl files by yourself **")
-    facets = ymport.stl(fileName+'.stl',color=(0,1,0),material=Steel)
+            print("** probably no internet connection, grab",sphereFile,"by yourself **")
+    sp=ymport.text(sphereFile,material=granularMaterial)
+    
+    hasInputWall = os.path.exists(wallFile)
+    if not hasInputWall:
+        print("Downloading mesh file",wallFile)
+        try:
+            os.system('wget http://yade-dem.org/publi/data/DEM8/'+wallFile)
+        except:
+            print("** probably no internet connection, grab",wallFile,"by yourself **")
+    facets = ymport.textFacets(wallFile,color=(0,1,0),material=Steel)
+    #facets = ymport.stl(fileName+'.stl',color=(0,1,0),material=Steel)
     fctIds = range(len(facets))
     
     O.bodies.append(facets)
-    O.bodies.append(sp_new)
+    O.bodies.append(sp)
         
     # -------------------------------------------------------------------- #
     # Count the number of spherical particles to verify sample size. We can comment this out later on.
@@ -149,7 +119,7 @@ if not mpi or mp.rank==0:
             numSpheres=numSpheres+1
     print('The total number of spheres is: ',numSpheres)
         
-    collider.verletDist = 0.6*particleRadius
+    collider.verletDist = 0.2*O.bodies[-1].shape.radius
     O.dt=0.6 * PWaveTimeStep()
     O.dynDt=False
 
@@ -162,7 +132,7 @@ def eraseEscapedParticles():
     ts=time.time()
     ers=[]
     for b in O.bodies:
-        if isinstance(b.shape,Sphere) and b.state.pos[2]<-z-20: # I do not delete the particles right after they pass the orifice, to disturb the simulation as little as possible
+        if isinstance(b.shape,Sphere) and b.state.pos[2]<-z: # I do not delete the particles right after they pass the orifice, to disturb the simulation as little as possible
             ers.append(b.id)
             count+=1            
     if mpi:
@@ -192,19 +162,19 @@ if mpi: # import and tune MPI module
     mp.DOMAIN_DECOMPOSITION =True
     mp.ACCUMULATE_FORCES=False
     mp.MERGE_W_INTERACTIONS=False
-    mp.REALLOCATE_MINIMAL = False # if true, intersections are minimized before reallocations, hence minimizing the number of reallocated bodies
+    mp.REALLOCATE_MINIMAL = False
     mp.REALLOCATE_FREQUENCY=20
     mp.USE_CPP_REALLOC=True
     mp.MINIMAL_INTERSECTIONS = True
-    mp.MASTER_UPDATE_STATES = False
-    mp.ERASE_REMOTE_MASTER = True
     mp.YADE_TIMING=reportTiming
+    mp.USE_CPP_INTERS = True
+    #mp.VERBOSE_OUTPUT=True
 else:
     O.timingEnabled=reportTiming
 
-substeps=500
+substeps=400
 while len(O.bodies)-numErased>0 or len(O.bodies)==0:
-#for k in range(3):
+#for k in range(0):
     t1=time.time()
     if mpi:
         mp.mpirun(substeps,numThreads,withMerge=True) # if numThreads=1 this will fall-back to normal O.run() and mp.rank=0
@@ -226,7 +196,7 @@ if opts.nogui==False:
 	from yade import qt
 	v=qt.View()
 
-	v.eyePosition = Vector3(0,-600,100)
+	v.eyePosition = Vector3(0,-.6,.1)
 	v.upVector    = Vector3(0,0,1)
 	v.viewDir     = Vector3(0,1,0)
 #	v.grid=(False,True,False)
