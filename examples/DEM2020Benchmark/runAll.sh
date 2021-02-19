7cfde252053ed64488134e210f1053c229d71295
bchareyre <bchareyre@gmail.com>
2021-02-19 15:49:30 +0100

    set permanent url to revision just before this one (edea1b5e219691753)

--- examples/DEM2020Benchmark/runAll.sh
+++ examples/DEM2020Benchmark/runAll.sh
@@ -11,7 +11,7 @@
 
 
 # That url should point to valid gitlab branch
-export YADE_BRANCH="https://gitlab.com/yade-dev/trunk/-/raw/99f912067159bf8a475ea217c8c46db94f67215d/examples/DEM2020Benchmark"
+export YADE_BRANCH="https://gitlab.com/yade-dev/trunk/-/raw/edea1b5e219691753662104bb7cfac0986536aed/examples/DEM2020Benchmark"
 # latest would be:
 # export YADE_BRANCH=https://gitlab.com/yade-dev/trunk/-/raw/master/examples//DEM2020Benchmark
 
