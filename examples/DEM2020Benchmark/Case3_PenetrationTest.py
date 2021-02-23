f754168a9a25da8efe76a9907f5d9ba8a495550f
Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
2021-02-23 19:59:35 +0000

    Case3: Facets from TUHH server; remove M2; download walls once.

--- examples/DEM2020Benchmark/Case3_PenetrationTest.py
+++ examples/DEM2020Benchmark/Case3_PenetrationTest.py
@@ -2,26 +2,28 @@
 # 2020 © Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
 # 2020 © Bruno Chareyre <bruno.chareyre@grenoble-inp.fr> 
 
-# Benchmark of basic performance of open-source DEM simulation systems
-# Case 3: Penetration test
 
-# for efficient production run with the following command (it will terminate automatically when done):
-#   'yadedaily -n -x Case3_PenetrationTest_SI.py 25000 M1'
-# for keeping GUI run like this:
-#   'yadedaily Case3_PenetrationTest_SI.py 25000 M1'
-# adapt yade executable if not 'daily' version and pick one in 25000,50000,10000
-# the input scripts are downloaded from TUHH as part of the script execution if not available in current path
+# Benchmark of basic performance of open-source DEM simulation systems
+# Case 3: Penetration Test
+# Units: SI (m, N, Pa, kg, sec)
+
+# script execution:
+#   Option 1:  'yadedaily -n -x Case3_PenetrationTest.py 25000'		# use this command for efficient production (no GUI; Yade will terminate automatically when the simulation is finished)
+#   Option 2:  'yadedaily Case3_PenetrationTest.py 25000'		# use this command to keep GUI (affects the overall performance)
+
+# adapt yade executable if not 'daily' version and pick one of 25000,50000,100000
+# the input data are downloaded from TUHH as part of the script execution if not available in current path
 # provided input should be in ./inputData relative to where yade is executed
 
-# Units: SI (m, N, Pa, kg, sec)
 # -------------------------------------------------------------------- #
-# Input Data -> Define Number of particles. Uncomment the prefered choice. The rest of the script should be automated for all different scenarios.
+# Input Data - Number of particles. The rest of the script is automated for all different scenarios.
+
 # additional arg, if any is used to define N, else 25k is default
 if len(sys.argv)>1: N=int(sys.argv[1])
 else: N=25000
 if N not in [25000,50000,100000]: print("input error, call 'yadedaily -n -x scriptName.py' or 'yadedaily -n -x scriptName.py 25000' (or 50000, etc.)")
 
-material=str(sys.argv[2]) if len(sys.argv)>2 else 'M1'
+material="M1"
 
 try:
     os.mkdir('inputData')
@@ -49,15 +51,15 @@ e_gs=e_M1_St	# Coefficient of restitution (e) between granular material (g) and 
 # -------------------------------------------------------------------- #
 # Load the initial sphere pack from .txt file
 
-inputFileName = 'inputData/'+str(int(N/1000))+'KParticles.txt'
-altName = 'inputData/'+str(int(N/1000))+'KParticlesSwapped.txt'
+inputFileName = 'inputData/Case3_'+str(int(N/1000))+'KParticles.txt'
+altName = 'inputData/Case3_'+str(int(N/1000))+'KParticlesSwapped.txt'
 
 urls = {}
 urls["25000"]="https://cloud.tuhh.de/index.php/s/9BHg3p39FzC7pYL/download"
 urls["50000"]="https://cloud.tuhh.de/index.php/s/28ETraDs3fbY3xo/download"
 urls["100000"]="https://cloud.tuhh.de/index.php/s/Pw2Tyc7Aw9g2kCB/download"
-#urls["walls"]="https://cloud.tuhh.de/index.php/s/49HZwje9zj4R78m/download"  # <======= NOT USED / Need a fix - we are using data from yade-dem  herefafter 
-urls["walls"]="http://yade-dem.org/publi/data/DEM8/Case3_PenetrationTest_Walls.txt"
+urls["walls"]="https://cloud.tuhh.de/index.php/s/49HZwje9zj4R78m/download"
+#urls["walls"]="http://yade-dem.org/publi/data/DEM8/Case3_PenetrationTest_Walls.txt"
 
 # download from organizer at TUHH
 if not os.path.exists(inputFileName):    
@@ -84,21 +86,34 @@ if not os.path.exists(altName):
     y.close()
 
 from yade import ymport
-sp=ymport.text(altName,material= (M1 if material=="M1" else M2))
-
+sp=ymport.text(altName,material=M1) #(M1 if material=="M1" else M2)
+                      
 sp[-1]=sphere(sp[-1].state.pos,sp[-1].shape.radius,material='Steel') # Redefine this with the correct material, as sp[-1] corresponds to the large sphere made of steel
 O.bodies.append(sp)
 
 # -------------------------------------------------------------------- #
 # Load facets making the box
-wallFileName = 'inputData/Case3Walls'+str(int(N/1000))+'.txt' # a bit pointless to download again with different N but it's to avoid collisions between parallel jobs
+wallFile = 'inputData/Case3_Walls.txt' #+str(int(N/1000))+'.txt' # Bruno: a bit pointless to download again with different N but it's to avoid collisions between parallel jobs # Vas: I think we do not need to download the same walls 3 times, in different files, even for parallel executions. If the walls exist from a previous execution, nothing goes wrong.
 
 # download from organizer at TUHH
-if not os.path.exists(wallFileName):    
-    os.system("wget -O "+wallFileName+" "+urls["walls"])
+hasInputWall = os.path.exists(wallFile)
+if not hasInputWall:
+    print("Downloading mesh file",wallFile)
+    try:
+        os.system('wget --no-clobber -O '+wallFile+'_temp '+urls["walls"])
+    except:
+        print("** probably no internet connection, grab",wallFile,"by yourself **")
+
+    with open(wallFile+'_temp') as x, open(wallFile, 'w') as y:
+        for line in x:
+            columns=line.split()
+            if columns[0]=='vertex1_X[m]': continue # skip first line of headers
+            if len(columns)<9: continue # trailing empty lines
+            y.write('\t'.join(columns)+'\n')
+    y.close()
 
 from yade import ymport
-facets = ymport.textFacets(wallFileName,color=(0,1,0),material=Steel)
+facets = ymport.textFacets(wallFile,color=(0,1,0),material=Steel)
 fctIds = range(len(facets))
 O.bodies.append(facets)
 
@@ -115,7 +130,7 @@ O.engines=[
 	InteractionLoop(
 		[Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
 		[Ip2_FrictMat_FrictMat_MindlinPhys(
-			en         = MatchMaker(matches=((1,1,e_gg),(0,1,e_gs)))  # 0 being the id of Steel and 1 being the id of M1 # ,(0,0,e_ss) # e_ss is not needed, as the steel ball is not supposed to touch the steel box. Was defined for debugging purposes above
+			en         = MatchMaker(matches=((1,1,e_gg),(0,1,e_gs)))  # 0 being the id of Steel and 1 being the id of M1 # ,(0,0,e_ss) # e_ss is not needed (Steel-Steel), as the steel ball is not supposed to touch the steel box.
 		)],
 		[Law2_ScGeom_MindlinPhys_Mindlin()],
 	),
