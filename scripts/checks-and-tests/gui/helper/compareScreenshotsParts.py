a4ec1ca1ee956913999d2bfdd534baf3e65a853d
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2021-06-05 18:39:49 +0200

    [GUI test] Reformat these files: autopep8 --max-line-length 160 --ignore=W191,E101,E111,E401 -i …………

--- scripts/checks-and-tests/gui/helper/compareScreenshotsParts.py
+++ scripts/checks-and-tests/gui/helper/compareScreenshotsParts.py
@@ -1,17 +1,19 @@
 import numpy as np
 import os, sys
-print('\033[93m Checking screenshots, paths: \n',sys.argv[-1],'\n',sys.argv[-2],' \033[0m')
+print('\033[93m Checking screenshots, paths: \n', sys.argv[-1], '\n', sys.argv[-2], ' \033[0m')
 try:
 	import cv2
 except:
-	print("'\033[93m'+Please install python3-opencv package for automated screenshot comparison. Screenshots for manual comparison are in directory: ",sys.argv[-1],'\033[0m')
+	print("'\033[93m'+Please install python3-opencv package for automated screenshot comparison. Screenshots for manual comparison are in directory: ", sys.argv[-1], '\033[0m')
 	sys.stdout.flush()
-	os._exit(0)	#os._exit(1) use 1 for error and 0 for ok
+	os._exit(0)  # os._exit(1) use 1 for error and 0 for ok
 
 
 screenshotNames = []
-thresholdDict = dict({'view': 60000, 'console': 110000, 'controller': 110000, 'inspector': 100000})	#Number of pixels which have to be different in order to draw differentiation
-					#FIXME: Current threshold is really high due to inspector and controller window sizes and contents differing between gui tests (and blinkHighlight on yellow)
+# Number of pixels which have to be different in order to get attention
+thresholdDict = dict({'view': 60000, 'console': 110000, 'controller': 110000, 'inspector': 100000})
+# FIXME: Current threshold is really high due to inspector and controller window sizes and contents differing between gui tests (and blinkHighlight on yellow)
+
 
 def printFlushExit(msg, code):
 	print(msg)
@@ -21,30 +23,31 @@ def printFlushExit(msg, code):
 
 def getPngNames(path):
 	pngList = []
-	files=os.listdir(path)
-	
+	files = os.listdir(path)
+
 	for png in files:
-		if(png[len(png)-4:]==".png"):	#we are looking for png's only thus we check if file is a .png
+		if(png[len(png) - 4:] == ".png"):  # we are looking for png's only thus we check if file is a .png
 			pngList.append(png)
-	
+
 	pngList.sort()
-	print("Path:  ",path)
-	print("Files: ",pngList)
+	print("Path:  ", path)
+	print("Files: ", pngList)
 	return pngList
 
-#Hhre we get the screenshotNames list
-screenshotNames = getPngNames(path=sys.argv[-1])	#path to screenshot's folder should be given as an argument to command calling this script
 
-i=0
-j=0
+# Hhre we get the screenshotNames list
+screenshotNames = getPngNames(path=sys.argv[-1])  # path to screenshot's folder should be given as an argument to command calling this script
+
+i = 0
+j = 0
 while i < len(screenshotNames):
-	print('\033[93m Checking screenshot ',screenshotNames[i],' \033[0m')
-	#We must only check if comparable file exists as screenshot file must exist if it is in the screenshotNames list as generation requirement
-	if not os.path.isfile(sys.argv[-2]+"/compareSources/"+screenshotNames[i]):
-		printFlushExit('\033[91m'+"ERROR: file, "+screenshotNames[i]+" does not exist in compareSources folder."'\033[0m', 1)
-	#screenshotNames list is used for comparable generation aswell as compareSources and screenshots both have the exact same filenames (this prevents wrong files comparison)
-	comparable = cv2.imread(sys.argv[-2]+"/compareSources/"+screenshotNames[i], cv2.IMREAD_COLOR)
-	screenshot = cv2.imread(sys.argv[-1]+"/"+screenshotNames[i], cv2.IMREAD_COLOR)
+	print('\033[93m Checking screenshot ', screenshotNames[i], ' \033[0m')
+	# We must only check if comparable file exists as screenshot file must exist if it is in the screenshotNames list as generation requirement
+	if not os.path.isfile(sys.argv[-2] + "/compareSources/" + screenshotNames[i]):
+		printFlushExit('\033[91m' + "ERROR: file, " + screenshotNames[i] + " does not exist in compareSources folder."'\033[0m', 1)
+	# screenshotNames list is used for comparable generation aswell as compareSources and screenshots both have the exact same filenames (this prevents wrong files comparison)
+	comparable = cv2.imread(sys.argv[-2] + "/compareSources/" + screenshotNames[i], cv2.IMREAD_COLOR)
+	screenshot = cv2.imread(sys.argv[-1] + "/" + screenshotNames[i], cv2.IMREAD_COLOR)
 	if screenshot.shape == comparable.shape:
 		view1 = comparable[0:560, 0:550]
 		console1 = comparable[560:1600, 0:550]
@@ -54,21 +57,25 @@ while i < len(screenshotNames):
 		console2 = screenshot[560:1600, 0:550]
 		controller2 = screenshot[0:1600, 550:1050]
 		inspector2 = screenshot[0:1600, 1050:]
-		comparableParts = [(view1,'view'), (console1,'console'), (controller1,'controller'), (inspector1,'inspector')]	#not really perfect solution for threshold-part synchronization
-		screenshotParts = [(view2,'view'), (console2,'console'), (controller2,'controller'), (inspector2,'inspector')]	#but better than depending on hardcoded order
+		# not perfect solution for threshold-part synchronization
+		# but better than depending on hardcoded order
+		comparableParts = [(view1, 'view'), (console1, 'console'), (controller1, 'controller'), (inspector1, 'inspector')]
+		screenshotParts = [(view2, 'view'), (console2, 'console'), (controller2, 'controller'), (inspector2, 'inspector')]
 
 		while j < len(screenshotParts):
 			if(screenshotParts[j][0].shape == comparableParts[j][0].shape):
 				partDiff = cv2.subtract(screenshotParts[j][0], comparableParts[j][0])
-				r,g,b = cv2.split(partDiff)
-				error = (cv2.countNonZero(r)+cv2.countNonZero(g)+cv2.countNonZero(b))/3	#arithmetic average of errors from all 3 colours (could try to replace with weighted average)
+				r, g, b = cv2.split(partDiff)
+				# arithmetic average of errors from all 3 colours (could try to replace with weighted average)
+				error = (cv2.countNonZero(r) + cv2.countNonZero(g) + cv2.countNonZero(b)) / 3
 
 				if(error > thresholdDict[screenshotParts[j][1]]):
-					printFlushExit("There are differences between: "+screenshotNames[j]+" "+screenshotParts[j][1]+" and "+screenshotNames[j]+" "+comparableParts[j][1]+"Pixels diffrentiating: "+error, 1)
+					printFlushExit("There are differences between: " + screenshotNames[j] + " " + screenshotParts[j][1]
+					               + " and " + screenshotNames[j] + " " + comparableParts[j][1] + "Pixels diffrent: " + error, 1)
 			else:
 				printFlushExit("can't compare pictures with different sizes", 1)
-			j+=1
+			j += 1
 	else:
 		printFlushExit("can't compare pictures with different sizes", 1)
 
-	i+=1
+	i += 1
--- scripts/checks-and-tests/gui/helper/readNames.py
+++ scripts/checks-and-tests/gui/helper/readNames.py
@@ -1,13 +1,13 @@
 import os, sys
 
-TESTS=""
+TESTS = ""
 
 path = sys.argv[-1]
-files=os.listdir(path)
+files = os.listdir(path)
 
 for file in files:
-	#Number 7 is the number of letters that we want to skip in current naming convention "testGui".
-	#-3 is the ending ".py" we do that because bash script needs only distinguishing names e.g. "Empty", "Simple"
-	if(file[len(file)-3:]==".py"):
-		TESTS+= file[7:-3]+" "
+	# Number 7 is the number of letters that we want to skip in current naming convention "testGui".
+	# -3 is the ending ".py" we do that because bash script needs only distinguishing names e.g. "Empty", "Simple"
+	if(file[len(file) - 3:] == ".py"):
+		TESTS += file[7:-3] + " "
 print(TESTS)
