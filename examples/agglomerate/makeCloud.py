69048a5993d08523d52f75b3241da174ef974f7a
clang-formatter <clang-formatter>
2021-08-10 16:04:34 +0200

    Python Reformat 4/18 - part of examples

--- examples/agglomerate/makeCloud.py
+++ examples/agglomerate/makeCloud.py
@@ -4,20 +4,20 @@
 ######################################################################
 from yade import export
 
-dim    = (15,15,15) # dimensions for makeCloud
+dim = (15, 15, 15)  # dimensions for makeCloud
 radius = 1
-fuzz   = 0.2
+fuzz = 0.2
 
 # use of makeCloud function
 sp = pack.SpherePack()
-sp.makeCloud((0,0,0), dim, rMean=radius, rRelFuzz=fuzz, seed=1)
+sp.makeCloud((0, 0, 0), dim, rMean=radius, rRelFuzz=fuzz, seed=1)
 sp.toSimulation()
 
 # save the result
 export.text('/tmp/cloud.txt')
 
 try:
-	from yade import qt
-	qt.View()
+    from yade import qt
+    qt.View()
 except:
-	pass
+    pass
