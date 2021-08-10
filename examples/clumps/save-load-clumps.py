ea0bcb6d579851402cbe39ff837f68973a731bfa
clang-formatter <clang-formatter>
2021-08-10 16:04:45 +0200

    Python Reformat 5/18 - part of examples

--- examples/clumps/save-load-clumps.py
+++ examples/clumps/save-load-clumps.py
@@ -4,67 +4,69 @@
 from __future__ import print_function
 
 from builtins import range
-from yade import pack,export,qt,ymport
+from yade import pack, export, qt, ymport
 
-#define material for all bodies:
-id_Mat=O.materials.append(FrictMat(young=1e6,poisson=0.3,density=1000,frictionAngle=1))
-Mat=O.materials[id_Mat]
+# define material for all bodies:
+id_Mat = O.materials.append(FrictMat(young=1e6, poisson=0.3, density=1000, frictionAngle=1))
+Mat = O.materials[id_Mat]
 
-#define engines:
-O.engines=[
-	ForceResetter(),
-	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Box_Aabb()]),
-	InteractionLoop(
-		[Ig2_Sphere_Sphere_ScGeom(),Ig2_Box_Sphere_ScGeom()],
-		[Ip2_FrictMat_FrictMat_FrictPhys()],
-		[Law2_ScGeom_FrictPhys_CundallStrack()]
-	),
-	NewtonIntegrator(damping=0.7,gravity=[0,0,-10])
+# define engines:
+O.engines = [
+    ForceResetter(),
+    InsertionSortCollider([Bo1_Sphere_Aabb(), Bo1_Box_Aabb()]),
+    InteractionLoop(
+        [Ig2_Sphere_Sphere_ScGeom(), Ig2_Box_Sphere_ScGeom()],
+        [Ip2_FrictMat_FrictMat_FrictPhys()],
+        [Law2_ScGeom_FrictPhys_CundallStrack()]
+    ),
+    NewtonIntegrator(damping=0.7, gravity=[0, 0, -10])
 ]
 
-#create a box:
-id_box = O.bodies.append(box((0,0,0),(2,2,.1),fixed=True,material=Mat))
+# create a box:
+id_box = O.bodies.append(box((0, 0, 0), (2, 2, .1), fixed=True, material=Mat))
 
 
-#### show how to use appendClumped():
+# show how to use appendClumped():
 
 
-#create 2 clumps:
-clump1=O.bodies.appendClumped([\
-sphere([0,0,1], material=Mat, radius=0.5),\
-sphere([0.2,0,1], material=Mat, radius=0.5)\
+# create 2 clumps:
+clump1 = O.bodies.appendClumped([
+    sphere([0, 0, 1], material=Mat, radius=0.5),
+    sphere([0.2, 0, 1], material=Mat, radius=0.5)
 ])
-clump2=O.bodies.appendClumped([\
-sphere([3,1,2], material=Mat, radius=0.5),\
-sphere([3.2,1,2], material=Mat, radius=0.5)\
+clump2 = O.bodies.appendClumped([
+    sphere([3, 1, 2], material=Mat, radius=0.5),
+    sphere([3.2, 1, 2], material=Mat, radius=0.5)
 ])
 
-#get clump ids:
+# get clump ids:
 id_clump1 = clump1[0]
 id_clump2 = clump2[0]
 
-#definition for getting informations from all clumps:
+# definition for getting informations from all clumps:
+
+
 def getClumpInfo():
-	for b in O.bodies:
-		if b.isClump:
-			print('Clump ',b.id,' has following members:')
-			keys = list(b.shape.members.keys())
-			for ii in range(0,len(keys)):
-				print('- Body ',keys[ii])
-			print('inertia:',b.state.inertia)
-			print('mass:',b.state.mass,'\n')
+    for b in O.bodies:
+        if b.isClump:
+            print('Clump ', b.id, ' has following members:')
+            keys = list(b.shape.members.keys())
+            for ii in range(0, len(keys)):
+                print('- Body ', keys[ii])
+            print('inertia:', b.state.inertia)
+            print('mass:', b.state.mass, '\n')
 
 
-#### show how to use addToClump():
+# show how to use addToClump():
 
-#create a new sphere:
-id_new=O.bodies.append(sphere([0,0.2,1], material=Mat, radius=0.5))
+# create a new sphere:
+id_new = O.bodies.append(sphere([0, 0.2, 1], material=Mat, radius=0.5))
 
-#add a sphere to the clump:
-O.bodies.addToClump([id_new],id_clump1)
+# add a sphere to the clump:
+O.bodies.addToClump([id_new], id_clump1)
 
-#add a clump to a clump:
-O.bodies.addToClump([id_clump2],id_clump1)
+# add a clump to a clump:
+O.bodies.addToClump([id_clump2], id_clump1)
 
 print('\nSTATE after adding the second clump to clump ------------')
 getClumpInfo()
@@ -72,12 +74,12 @@ getClumpInfo()
 print("Save clumps")
 export.textClumps("savedClumps.txt")
 print("Load clumps")
-ymport.textClumps("savedClumps.txt", shift=Vector3(0,0,1.5))
+ymport.textClumps("savedClumps.txt", shift=Vector3(0, 0, 1.5))
 
 print("After saving")
 getClumpInfo()
 
-O.dt=1e-6
+O.dt = 1e-6
 
 print('\nPress Play button ... ')
 renderer = qt.Renderer()
--- examples/concrete/interaction-histogram.py
+++ examples/concrete/interaction-histogram.py
@@ -2,40 +2,42 @@
 #
 # demonstration of the yade.post2d module (see its documentation for details)
 #
-import pylab # the matlab-like interface of matplotlib
-pylab.ioff()
-import numpy
 import os.path
+import numpy
+import pylab  # the matlab-like interface of matplotlib
+pylab.ioff()
 
 # run uniax.py to get this file
-loadFile='/tmp/uniax-tension.yade.gz'
-if not os.path.exists(loadFile): raise RuntimeError("Run uniax.py first so that %s is created"%loadFile)
+loadFile = '/tmp/uniax-tension.yade.gz'
+if not os.path.exists(loadFile):
+    raise RuntimeError("Run uniax.py first so that %s is created" % loadFile)
 O.load(loadFile)
 
 # axis normal to the plane in which we do the histogram
-axis=0 # x, i.e. plot the yz plane
-ax1,ax2=(axis+1)%3,(axis+2)%3 ## get the other two indices, i.e. 1 and 2 in this case
-angles,forces=[],[]
+axis = 0  # x, i.e. plot the yz plane
+ax1, ax2 = (axis + 1) % 3, (axis + 2) % 3  # get the other two indices, i.e. 1 and 2 in this case
+angles, forces = [], []
 for i in O.interactions:
-	if not i.isReal: continue
-	norm=i.geom.normal
-	angle=atan(norm[ax2]/norm[ax1])
-	force=i.phys.normalForce.norm()
-	angles.append(angle)
-	forces.append(force)
+    if not i.isReal:
+        continue
+    norm = i.geom.normal
+    angle = atan(norm[ax2] / norm[ax1])
+    force = i.phys.normalForce.norm()
+    angles.append(angle)
+    forces.append(force)
 # easier: plain histogram
-#pylab.hist(angles,weights=forces,bins=20)
+# pylab.hist(angles,weights=forces,bins=20)
 
 
 # polar histogram
 pylab.figure()
-## prepare data
-values,bins=numpy.histogram(angles,weights=forces,bins=20)
-## prepare polar plot
-pylab.subplot(111,polar=True);
-## plot bar chart, with the histogram data
-### bins has one edge extra, remove it: [:-1]
-pylab.bar(left=bins[:-1],height=values,width=.7*pi/20);
+# prepare data
+values, bins = numpy.histogram(angles, weights=forces, bins=20)
+# prepare polar plot
+pylab.subplot(111, polar=True)
+# plot bar chart, with the histogram data
+# bins has one edge extra, remove it: [:-1]
+pylab.bar(left=bins[:-1], height=values, width=.7 * pi / 20)
 
 
 # predefined function
