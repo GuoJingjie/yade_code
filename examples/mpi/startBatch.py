db6c7ce3cf0b10033aa4cea6253faae504c17882
clang-formatter <clang-formatter>
2021-08-10 16:08:13 +0200

    Python Reformat 7/18 - part of examples

--- examples/mpi/startBatch.py
+++ examples/mpi/startBatch.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 threadsMax = 10
 threadsInterval = 1
-threadsList = np.linspace(2,threadsMax,num=threadsMax/threadsInterval) # need at least one master and one worker
+threadsList = np.linspace(2, threadsMax, num=threadsMax / threadsInterval)  # need at least one master and one worker
 
 for numThreads in threadsList:
-	#os.environ["IDENTIFIER"]='strongScalability-'+str(int(numThreads))+'-threads'
-	subprocess.call(['mpiexec','-n',str(int(numThreads)), 'yadempi-daily', '-x', '-n', 'testMPI_strongScalability.py', str(int(numThreads))]) 		
+    # os.environ["IDENTIFIER"]='strongScalability-'+str(int(numThreads))+'-threads'
+    subprocess.call(['mpiexec', '-n', str(int(numThreads)), 'yadempi-daily', '-x', '-n', 'testMPI_strongScalability.py', str(int(numThreads))])
