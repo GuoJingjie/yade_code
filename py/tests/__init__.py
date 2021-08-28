1cba1ea9d02bdd5396a3b0a9f2e1ba4c037a97cf
clang-formatter <clang-formatter>
2021-08-28 14:46:30 +0200

    Python yapf3 reformat: scripts/python-formatter.sh ./py/tests/

--- py/tests/__init__.py
+++ py/tests/__init__.py
@@ -1,35 +1,42 @@
 # encoding: utf-8
 # 2009 © Václav Šmilauer <eudoxos@arcig.cz>
 """All defined functionality tests for yade."""
-import unittest,inspect,sys
+import unittest, inspect, sys
 
 # add any new test suites to the list here, so that they are picked up by testAll
-allTests=[
-	  'dummyTest'
-	, 'wrapper'
-	, 'core'
-	, 'pbc'
-	, 'clump'
-	, 'cohesive-chain'
-	, 'engines'
-	, 'utilsModule'
-	, 'libVersions'
-        , 'testMinieigenHP'
-        , 'testMath'
-        , 'enumTest'
-	#, 'testColliderByGiulia' # to investigate later maybe, my impression is that due to issue #7 the results are randomly different in that ill-posed periodic problem
+# yapf: disable
+allTests = [
+   'dummyTest'
+ , 'wrapper'
+ , 'core'
+ , 'pbc'
+ , 'clump'
+ , 'cohesive-chain'
+ , 'engines'
+ , 'utilsModule'
+ , 'libVersions'
+ , 'testMinieigenHP'
+ , 'testMath'
+ , 'enumTest'
+        #, 'testColliderByGiulia' # to investigate later maybe, my impression is that due to issue #7 the results are randomly different in that ill-posed periodic problem
 ]
+# yapf: enable
 
 # add any new yade module (ugly...)
-import yade.export,yade.linterpolation,yade.log,yade.pack,yade.plot,yade.post2d,yade.timing,yade.utils,yade.ymport,yade.geom,yade.gridpfacet,yade.libVersions,yade.mpy
-allModules=(yade.export,yade.linterpolation,yade.log,yade.pack,yade.plot,yade.post2d,yade.timing,yade.utils,yade.ymport,yade.geom,yade.gridpfacet,yade.libVersions,yade.mpy)
+import yade.export, yade.linterpolation, yade.log, yade.pack, yade.plot, yade.post2d, yade.timing, yade.utils, yade.ymport, yade.geom, yade.gridpfacet, yade.libVersions, yade.mpy
+allModules = (
+        yade.export, yade.linterpolation, yade.log, yade.pack, yade.plot, yade.post2d, yade.timing, yade.utils, yade.ymport, yade.geom, yade.gridpfacet,
+        yade.libVersions, yade.mpy
+)
 try:
 	import yade.qt
-	allModules+=(yade.qt,)
-except ImportError: pass
+	allModules += (yade.qt,)
+except ImportError:
+	pass
 
 # fully qualified module names
-allTestsFQ=['yade.tests.'+test for test in allTests]
+allTestsFQ = ['yade.tests.' + test for test in allTests]
+
 
 def testModule(module):
 	"""Run all tests defined in the module specified, return TestResult object
@@ -38,8 +45,9 @@ def testModule(module):
 
 	@param module: fully-qualified module name, e.g. yade.tests.wrapper
 	"""
-	suite=unittest.defaultTestLoader.loadTestsFromName(module)
-	return unittest.TextTestRunner(stream=sys.stdout,verbosity=2).run(suite)
+	suite = unittest.defaultTestLoader.loadTestsFromName(module)
+	return unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
+
 
 # https://docs.python.org/3/library/unittest.html
 # https://stackoverflow.com/questions/10099491/how-does-pythons-unittest-module-detect-test-cases
@@ -49,14 +57,15 @@ def testModule(module):
 def testAll():
 	"""Run all tests defined in all yade.tests.* modules and return
 	TestResult object for further examination."""
-	suite=unittest.defaultTestLoader.loadTestsFromNames(allTestsFQ)
+	suite = unittest.defaultTestLoader.loadTestsFromNames(allTestsFQ)
 	import doctest
-	if(yade.math.needsMpmathAtN(1) == False):
+	if (yade.math.needsMpmathAtN(1) == False):
 		# docstest checks the printed output. So high precision output causes failures such as:   Expected: 2000.0    Got: mpf('2000.0')
 		# todo: see if mp.pretty = True, from http://mpmath.org/doc/current/basics.html#printing can alleviate this.
 		for mod in allModules:
 			suite.addTest(doctest.DocTestSuite(mod))
 	else:
-		print("Note: docstest are skipped because it checks the printed output, instead of comparing numbers. High precision output causes failures such as: Expected: 2000.0 Got: mpf('2000.0')")
-	return unittest.TextTestRunner(stream=sys.stdout,verbosity=2).run(suite)
-
+		print(
+		        "Note: docstest are skipped because it checks the printed output, instead of comparing numbers. High precision output causes failures such as: Expected: 2000.0 Got: mpf('2000.0')"
+		)
+	return unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
--- py/tests/enumTest.py
+++ py/tests/enumTest.py
@@ -1,13 +1,16 @@
-import unittest,yade
+import unittest, yade
+
 
 class TestEnum(unittest.TestCase):
+
 	def setUp(self):
-		if self.canTest(): # to test if assignment of enums works properly we need any kind enum.
+		if self.canTest():  # to test if assignment of enums works properly we need any kind enum.
 			self.r = yade.OpenGLRenderingEngine()
 		else:
 			pass
+
 	def tearDown(self):
-		pass # here finalize the tests, maybe close an opened file, or something else
+		pass  # here finalize the tests, maybe close an opened file, or something else
 
 	def canTest(self):
 		return 'QT5' in yade.config.features
@@ -19,15 +22,15 @@ class TestEnum(unittest.TestCase):
 		self.r.blinkHighlight = 'incorrect string'
 
 	def testException(self):
-		if(not self.canTest()):
+		if (not self.canTest()):
 			return
 		# make sure it's impossible to assign incorrect values.
 		print("\n  Here it must throw two exceptions:")
-		self.assertRaises(TypeError,self.thisTestsException1)
-		self.assertRaises(TypeError,self.thisTestsException2)
+		self.assertRaises(TypeError, self.thisTestsException1)
+		self.assertRaises(TypeError, self.thisTestsException2)
 
 	def testAssignment(self):
-		if(not self.canTest()):
+		if (not self.canTest()):
 			return
 		# This is not to test if useSolver by default equals CholeskyFactorization, if you need to change the default then change the test here. Or test a different eum.
 		# Because this is to test function ArbitraryEnum_from_python<…> if assignment to any enum works as expected and if it throws when it should.
@@ -36,4 +39,3 @@ class TestEnum(unittest.TestCase):
 		self.assertTrue(self.r.blinkHighlight == yade.EnumClass_BlinkHighlight.NEVER)
 		self.r.blinkHighlight = 2
 		self.assertTrue(self.r.blinkHighlight == yade.EnumClass_BlinkHighlight.WEAK)
-
