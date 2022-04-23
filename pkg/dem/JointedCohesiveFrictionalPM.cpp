375d7b88bf383f38394f5548da6fa56c1444bff4
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2022-04-23 09:36:38 +0200

    remove redundant declaration of a YADE_PLUGIN

--- pkg/dem/JointedCohesiveFrictionalPM.cpp
+++ pkg/dem/JointedCohesiveFrictionalPM.cpp
@@ -18,7 +18,7 @@ namespace yade { // Cannot have #include directive inside.
 using math::max;
 using math::min; // using inside .cpp file is ok.
 
-YADE_PLUGIN((JCFpmMat)(JCFpmState)(JCFpmPhys)(Ip2_JCFpmMat_JCFpmMat_JCFpmPhys)(Law2_ScGeom_JCFpmPhys_JointedCohesiveFrictionalPM)(ThermalState));
+YADE_PLUGIN((JCFpmMat)(JCFpmState)(JCFpmPhys)(Ip2_JCFpmMat_JCFpmMat_JCFpmPhys)(Law2_ScGeom_JCFpmPhys_JointedCohesiveFrictionalPM));
 
 /********************** Law2_ScGeom_JCFpmPhys_JointedCohesiveFrictionalPM ****************************/
 CREATE_LOGGER(Law2_ScGeom_JCFpmPhys_JointedCohesiveFrictionalPM);
