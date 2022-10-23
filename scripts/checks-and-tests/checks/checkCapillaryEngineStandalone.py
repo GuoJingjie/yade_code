829a52d5a56aa93d24f30b8370bd10832357efc5
Bruno Chareyre <bruno.chareyre@grenoble-inp.fr>
2022-10-24 00:46:56 +0200

    skip CapillaryEngine check if CGAL not in features

--- scripts/checks-and-tests/checks/checkCapillaryEngineStandalone.py
+++ scripts/checks-and-tests/checks/checkCapillaryEngineStandalone.py
@@ -1,10 +1,13 @@
 #author: bruno.chareyre@grenoble-inp.fr
-import os
-os.system("wget -O capillaryfile.txt https://gitlab.com/yade-dev/yade-data/-/raw/main/capillaryFiles/capillaryfile.txt?inline=false")
-# test standalone bridge interpolator
-l=CapillarityEngine()
-l.liquidTension = 1
+if "CGAL" in features:
+    import os
+    os.system("wget -O capillaryfile.txt https://gitlab.com/yade-dev/yade-data/-/raw/main/capillaryFiles/capillaryfile.txt?inline=false")
+    # test standalone bridge interpolator
+    l=CapillarityEngine()
+    l.liquidTension = 1
 
-i = l.solveStandalone(1,1,1,0.1) # dummy solve to trigger triangulation
-if abs(i.vMeniscus - 0.22317611410950786) > 1e-10: raise YadeCheckError("capillary bridge volume changed")
-if abs(i.fCap[0] - 3.97627790531559322) > 1e-10: raise YadeCheckError("capillary force changed")
+    i = l.solveStandalone(1,1,1,0.1) # dummy solve to trigger triangulation
+    if abs(i.vMeniscus - 0.22317611410950786) > 1e-10: raise YadeCheckError("capillary bridge volume changed")
+    if abs(i.fCap[0] - 3.97627790531559322) > 1e-10: raise YadeCheckError("capillary force changed")
+else:
+    print("checkCapillaryEngineStandalone.py skipped because CGAL is not in features.")
